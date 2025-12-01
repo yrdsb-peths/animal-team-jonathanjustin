56a628263c156d7e76136daadd19fd50a080c99e
just1ngli <justinli99.ca@gmail.com>
2025-12-01 13:48:04 -0500

    set up myworld and label

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,60 @@
 import greenfoot.*;
 
-public class MyWorld extends World {
+public class MyWorld extends World
+{
+    public int score = 0;
+    Label scoreLabel;
+    int level = 1;
+    
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1, false);
+        
+        // Create the elephant object
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 300);
+        
+        // Create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
+    }
+    
+    /**
+     * End the game and draw 'GameOver'
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    /**
+     * Increase score
+     */
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+        
+        if(score % 5 == 0)
+        {
+            level += 1;
+        }
+    }
+    
+    /**
+     * Create a new apple at a random location at the top of screen
+     */
+    public void createApple()
+    
+    {
+        Apple apple = new Apple();
+        apple.setSpeed(level);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
+
