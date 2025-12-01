92135faf6e4aec929dda20a9270d7ae79d74ff49
just1ngli <justinli99.ca@gmail.com>
2025-12-01 13:37:54 -0500

    added apple

---
+++ Apple.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Food for our elephant.
+ * 
+ * @author Justin Li
+ * @version November 2025
+ */
+public class Apple extends Actor
+{
+    int speed = 1;
+    
+    public void act()
+    {
+        // Add your action code here.
+        int x = getX();
+        int y = getY() + speed;
+        setLocation(x, y);
+        
+        // Remove apple and draw game over when apple gets to bottom
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
+    }
+}
