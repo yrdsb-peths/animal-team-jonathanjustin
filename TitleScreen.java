3c6e7c8047549d763d351fde7c15c6f4b85c3f6b
just1ngli <justinli99.ca@gmail.com>
2025-12-02 13:57:05 -0500

    finished part 3
    Justin

---
+++ TitleScreen.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("The Elephant", 80);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, 192);
+        prepare();
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+    
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 87);
+        Label label = new Label("Use /u2190 and /u2192 to Move", 40);
+        addObject(label, 300, 273);
+        Label label2 = new Label("Press <space> to start", 40);
+        addObject(label2, 300, 331);
+    }
+}
